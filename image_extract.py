0093c18a22920fc55b3b650f2fd07fe47bd6211f
zfhrp <zfhrp6@users.noreply.github.com>
2021-08-21 13:45:18 +0900

    画像をDBから抽出するスクリプト

---
+++ image_extract.py
@@ -0,0 +1,28 @@
+import os, mysql.connector
+from sqlalchemy.pool import QueuePool
+
+mysql_connection_env = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'isucon',
+    'password': 'isucon',
+    'database': 'isucondition',
+    'time_zone': '+09:00',
+}
+
+cnxpool = QueuePool(lambda: mysql.connector.connect(**mysql_connector_env), pool_size=10)
+cur = cnx.cursor(dictionary=True)
+
+cur.execute('SELECT `jia_user_id`, `jia_isu_uuid`, `name`, `image` from `isu`')
+a = cur.fetchall()
+
+SAVE_PATH = '/home/isucon/isu_images'
+
+for im in a:
+    print(im.keys())
+    if not os.path.exists(f'{SAVE_PATH}/{im.get("jia_user_id")}'):
+        os.mkdir(f'{SAVE_PATH}/{im.get("jia_user_id")}')
+
+    with open(f'{SAVE_PATH}/{im.get("jia_user_id")}/{im.get("jia_isu_uuid")}.jpg', 'wb') as img:
+        img.write(im.get('image'))
+
